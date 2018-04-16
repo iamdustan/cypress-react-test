f1a7bdb42295e9011915444899db04f5abd72978
Josh Cypher <josh@webflow.com>
2018-04-16 10:22:12 -0500

    go click on somethign else

--- cypress/integration/input_spec.js
+++ cypress/integration/input_spec.js
@@ -15,6 +15,7 @@ describe('Kitchen Sink', function () {
         cy.visit('localhost:3000')
   
         cy.get('#root > div > div:nth-child(4) > input[type="number"]').type('1.9');
+        cy.get('#root > div > div:nth-child(3) > input[type="text"]').click();
         cy.get('#root > div > div:nth-child(4) > input[type="number"]')
           .then( element => {
               expect(element).to.have.attr('value', '1.9');
