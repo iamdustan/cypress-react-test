189780f92eda07eb673972ec2b397fce44988caf
Josh Cypher <josh@webflow.com>
2018-04-16 10:29:12 -0500

    naming things properly

--- cypress/integration/input_spec.js
+++ cypress/integration/input_spec.js
@@ -1,5 +1,5 @@
-describe('Kitchen Sink', function () {
-    it('should fill the text box', function () {
+describe('Why are inputs this hard?', function () {
+    it('Fill the input[type="text"] box', function () {
     
       cy.visit('localhost:3000')
 
@@ -10,12 +10,12 @@ describe('Kitchen Sink', function () {
         });
     });
 
-    it('should fill the text box', function () {
+    it('Fill the input[type="number"] box', function () {
     
         cy.visit('localhost:3000')
   
         cy.get('#root > div > div:nth-child(4) > input[type="number"]').type('1.9');
-        cy.get('#root > div > div:nth-child(3) > input[type="text"]').click();
+        cy.get('#root > div > div:nth-child(3) > input[type="text"]').click(); // comment me out to see failure
         cy.get('#root > div > div:nth-child(4) > input[type="number"]')
           .then( element => {
               expect(element).to.have.attr('value', '1.9');
