4e26d9835f885e34ec20ed0ad6a0a5623aaa210e
Dustan Kasten <dustan.kasten@gmail.com>
2018-04-16 12:04:48 -0400

    check it

--- cypress/integration/input_spec.js
+++ cypress/integration/input_spec.js
@@ -1,24 +1,19 @@
-describe('Why are inputs this hard?', function () {
-    it('Fill the input[type="text"] box', function () {
-    
-      cy.visit('localhost:3000')
+describe('Why are inputs this hard?', function() {
+  it('Fill the input[type="text"] box', function() {
+    cy.visit('localhost:3000');
 
-      cy.get('#root > div > div:nth-child(3) > input[type="text"]').type('1.7');
-      cy.get('#root > div > div:nth-child(3) > input[type="text"]')
-        .then( element => {
-            expect(element).to.have.attr('value', '1.7');
-        });
-    });
+    cy.get('input[name="text"]').type('1.7');
+    cy
+      .get('input[name="text"]')
+      .should('have.value', '1.7');
+  });
 
-    it('Fill the input[type="number"] box', function () {
-    
-        cy.visit('localhost:3000')
-  
-        cy.get('#root > div > div:nth-child(4) > input[type="number"]').type('1.9');
-        cy.get('#root > div > div:nth-child(3) > input[type="text"]').click(); // comment me out to see failure
-        cy.get('#root > div > div:nth-child(4) > input[type="number"]')
-          .then( element => {
-              expect(element).to.have.attr('value', '1.9');
-          });
-      })
+  it('Fill the input[type="number"] box', function() {
+    cy.visit('localhost:3000');
+
+    cy.get('input[name="number"]').type('1.9');
+    cy
+      .get('input[type="number"]')
+      .should('have.value', '1.9');
+  });
 });
